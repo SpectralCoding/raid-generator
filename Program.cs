465c68fa1ffb20c004c596bd956159a027201fd0
Caesar Kabalan <Dandelock@gmail.com>
2011-03-17 01:05:39 +0000

    git-svn-id: file:///content/svn/csharp/raidgenerator@2 3de2ddcc-0996-482e-8d88-eaa4498fcda7

--- Program.cs
+++ Program.cs
@@ -7,17 +7,27 @@ namespace RAIDGenerator {
 	class Program {
 		static void Main(string[] args) {
 			RaidController RaidController;
+			bool AutoRun = true;
 
 			Console.WriteLine("Welcome to the SpectralCoding RAID Generator!");
 			Console.WriteLine("\"Disk\" to be converted to raid: .\\Workspace\\utorrent.exe");
-			Console.WriteLine("Drive Bay: .\\Workspace\\Disks\\\n");
+			Console.WriteLine("Drive Bay: .\\Workspace\\Disks\\");
 
-			Console.Write("Please enter a RAID Mode: ");
+			if (AutoRun) {
+				RaidController = new RaidController("0", 3, 1);
+			} else {
+				Console.Write("Please enter a RAID Mode: ");
+				string RaidMode = Console.ReadLine();
+				Console.Write("Please enter the number of disks in the array: ");
+				int NumOfDisks = Convert.ToInt32(Console.ReadLine());
+				Console.Write("Please enter the size of the disks (in MB): ");
+				int DiskSize = Convert.ToInt32(Console.ReadLine());
+				RaidController = new RaidController(RaidMode, NumOfDisks, DiskSize);
+			}
 
-			RaidController = new RaidController(Console.ReadLine());
+			Console.WriteLine("\n" + RaidController.DisksInArray.ToString() + " Disk Raid Mode Set: " + RaidController.RaidMode.Description + "\n");
 
-			Console.WriteLine("\nRaid Mode Set: " + RaidController.RaidMode.Description);
-
+			RaidController.GenerateArray(@"C:\Users\Administrator\Desktop\RAID Generator\Workspace\utorrent.exe");
 
 			Console.Write("\nPress ENTER to exit... ");
 			Console.ReadLine();
--- RaidController.cs
+++ RaidController.cs
@@ -8,33 +8,76 @@ namespace RAIDGenerator {
 	class RaidController {
 
 		private RaidMode m_RaidMode;
-		private bool[] m_SourceDisk;
-		private bool[][] m_DiskArray;
+		private int m_DisksInArray;
+		private int m_DiskSize;
 
 		public RaidMode RaidMode {
 			get { return m_RaidMode; }
 		}
-
-		public RaidController(string inputRaidMode) {
-			m_RaidMode = new RaidMode(inputRaidMode);
-			ReadSourceDisk(@"C:\Users\Administrator\Desktop\RAID Generator\Workspace\utorrent.exe");
-			SplitIntoDisks();
-			WriteDisks();
+		public int DisksInArray {
+			get { return m_DisksInArray; }
+		}
+		public int DiskSize {
+			get { return m_DiskSize; }
 		}
 
-		private void SplitIntoDisks() {
+		public RaidController(string inputRaidMode, int inputDisksInArray, int DiskSizeMB) {
+			m_RaidMode = new RaidMode(inputRaidMode);
+			m_DisksInArray = inputDisksInArray;
+			m_DiskSize = DiskSizeMB * 8388608;
+		}
+
+		public void GenerateArray(string inputSourceDiskFilename) {
+			bool[] SourceDisk;
+			bool[][] DiskArray;
+			SourceDisk = FileToBoolArray(inputSourceDiskFilename);
+			DiskArray = SplitIntoDisks(SourceDisk);
+			WriteDisks(DiskArray);
+		}
+
+		public void RecombineArray() {
 			switch (m_RaidMode.Title) {
 				case "RAID0":
-
+					#region RAID0
 					break;
+					#endregion
 			}
 		}
 
-		private void WriteDisks() {
-
+		private bool[][] SplitIntoDisks(bool[] inputDisk) {
+			bool[][] returnBoolArray = null;
+			switch (m_RaidMode.Title) {
+				case "RAID0":
+					#region RAID0
+					int UsedDisks = m_DisksInArray;
+					returnBoolArray = new bool[UsedDisks][];
+					for (int i = 0; i < returnBoolArray.Length; i++) {
+						returnBoolArray[i] = new bool[m_DiskSize];
+					}
+					int CurrentDisk = 0;
+					int CurrentBlock = 0;
+					for (int i = 0; i < inputDisk.Length; i++) {
+						returnBoolArray[CurrentDisk][CurrentBlock] = inputDisk[i];
+						CurrentDisk++;
+						if (CurrentDisk == UsedDisks) {
+							CurrentDisk = 0;
+							CurrentBlock++;
+						}
+					}
+					break;
+					#endregion
+			}
+			return returnBoolArray;
 		}
 
-		private void ReadSourceDisk(string inputSourceDiskFilename) {
+		private void WriteDisks(bool[][] inputDiskArray) {
+			for (int i = 0; i < inputDiskArray.Length; i++) {
+				BoolArrayToFile(inputDiskArray[i], @"C:\Users\Administrator\Desktop\RAID Generator\Workspace\Disks\Disk" + i + ".dsk");
+			}
+		}
+
+		public bool[] FileToBoolArray(string inputSourceDiskFilename) {
+			Console.WriteLine("Reading {0}...", Path.GetFileName(inputSourceDiskFilename));
 			byte[] tempHexDisc = null;
 			string tempBinaryStr = String.Empty;
 			int discPos = 0;
@@ -42,28 +85,30 @@ namespace RAIDGenerator {
 			BinaryReader tempBR = new BinaryReader(tempFS);
 			long fileLength = new FileInfo(inputSourceDiskFilename).Length;
 			tempHexDisc = tempBR.ReadBytes((int)fileLength);
-			m_SourceDisk = new bool[tempHexDisc.Length * 8];
+			bool[] returnBoolArr = new bool[tempHexDisc.Length * 8];
 			for (int i = 0; i < tempHexDisc.Length; i++) {
 				tempBinaryStr = Convert.ToString(tempHexDisc[i], 2).PadLeft(8, '0'); ;
 				for (int j = 0; j < 8; j++) {
 					if (tempBinaryStr.Substring(j, 1) == "1") {
-						m_SourceDisk[discPos] = true;
+						returnBoolArr[discPos] = true;
 					} else {
-						m_SourceDisk[discPos] = false;
+						returnBoolArr[discPos] = false;
 					}
 					discPos++;
 				}
 			}
+			return returnBoolArr;
 		}
 
-		private void BoolArrayToFile(string inputOutputFile) {
+		private void BoolArrayToFile(bool[] inputBoolArr, string inputOutputFile) {
+			Console.WriteLine("Writing {0}...", Path.GetFileName(inputOutputFile));
 			FileStream fs = File.Create(inputOutputFile);
 			UTF8Encoding utf8 = new UTF8Encoding();
 			BinaryWriter bw = new BinaryWriter(fs, utf8);
 			string tempBinaryStr = String.Empty;
-			for (int i = 0; i < m_SourceDisk.Length; i += 8) {
+			for (int i = 0; i < inputBoolArr.Length; i += 8) {
 				for (int j = i; j < i + 8; j++) {
-					if (m_SourceDisk[j]) {
+					if (inputBoolArr[j]) {
 						tempBinaryStr += "1";
 					} else {
 						tempBinaryStr += "0";
